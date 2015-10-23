ca5ae1ab0890cd59e15915f053cd41887092f3db
Liz Sander <liz.sander91@gmail.com>
2015-10-23 11:56:29 -0500

    Code to install all the R packages locally

---
+++ Code/InstallAllPackages/InstallAPackage.R
@@ -0,0 +1,37 @@
+# read command line arguments:
+# The program can be launched with
+# Rscript package_name install
+# Rscript package_name remove
+
+options(echo=TRUE) # if you want see commands in output file
+args <- commandArgs(trailingOnly = TRUE)
+package_name <- args[1]
+flag <- args[2]
+
+if (flag == "install"){
+  if (eval(substitute(require(pkg), list(pkg = as.name(package_name))))){
+  #if (require(package_name, character.only = TRUE)){
+    # the package is already installed
+    print(paste("package", package_name, "is already installed"))
+  } else {
+    # try installing the package
+    eval(substitute(install.packages(pkg, contriburl  = "https://cran.rstudio.com/src/contrib"), list(pkg = (package_name))))
+  }
+  # now make sure that everything is fine
+  if (require(package_name, character.only = TRUE)){
+    # append to the success files
+    system(paste("echo", package_name, ">> success.txt"))
+  } else {
+    system(paste("echo", package_name, ">> failure.txt"))
+    print("could not install package!")
+  }
+}
+
+if (flag == "remove"){
+  if (eval(substitute(require(pkg), list(pkg = as.name(package_name))))){
+    # the package is already installed, remove
+    eval(substitute(remove.packages(pkg), list(pkg = (package_name))))
+  } else {
+    print(paste("package", package_name, "is not installed -- cannot remove"))
+  }
+}
---
+++ Code/InstallAllPackages/InstallAllPackages.R
@@ -0,0 +1,22 @@
+# This installs all R packages
+# Because there are nested dependencies, the first run will install all stand-alone packages
+# the second all those depending on stand-alone, and so on.
+how_many_runs <- 1
+
+# retrieve all the package names
+all_packages <- as.vector(available.packages(contriburl  = "https://cran.rstudio.com/src/contrib")[,"Package"])
+
+# cleanup
+system("rm success.txt")
+system("rm failure.txt")
+
+processing <- 1
+for (package_name in all_packages){
+  # call the script
+  print("##############################################################################")
+  print(processing)
+  print("##############################################################################")
+  system(paste("Rscript InstallAPackage.R", package_name, "install"))
+  processing <- processing + 1 
+  if (processing > 10) break
+}
