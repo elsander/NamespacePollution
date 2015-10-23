2a5ae4fbded650e538af78b2d32b894b913357a3
Liz Sander <liz.sander91@gmail.com>
2015-10-23 18:27:04 -0500

    Started writing a function to generate the data. There are a number of problems, but a small dataset can be found in Data * some packages do not load properly * some packages are installed multiple times (?) * there are some inconsistencies, but the bulk is there

---
+++ Code/RCode/generate_data.R
@@ -0,0 +1,47 @@
+# NOTES:
+# For some reason, I call the same package twice
+# moreover, I have installed some packages more than once?
+# finally, some packages do not load
+
+
+# for each of available packages,
+# extract the name of all the functions
+# saves them to an adjacency list
+
+# packages that are loaded automatically
+special_packages <- c("grDevices", "methods", "stats", "utils", "graphics", "datasets", "base")
+                      
+get_functions_from_package <- function(package_name){
+  # load the package
+  eval(substitute(require(pkg), list(pkg = as.name(package_name))))
+  # extract the functions
+  my_funcs <- as.vector(lsf.str(paste("package:", package_name, sep = "")))
+  # unload the package
+  if (package_name %in% special_packages == FALSE){
+    detach(paste("package:", package_name, sep = ""), character.only = TRUE)
+  }
+  if (length(my_funcs) > 0){
+    return(data.frame(package_name = package_name, function_name = my_funcs))
+  }
+  else{
+    return(data.frame(package_name = character(0), function_name = character(0)))
+  }
+}
+
+# get all installed packages
+my_installed_packages <- unique(as.vector(installed.packages()[,"Package"]))
+# add packages that are loaded automatically
+my_installed_packages <- c(special_packages, my_installed_packages)
+
+blacklist <- c("abbyyR")
+
+# finally, compile the data
+function_names <- data.frame()
+for (pkg in my_installed_packages){
+  print(pkg)
+  if (pkg %in% blacklist == FALSE){
+    try(function_names <- rbind(function_names, get_functions_from_package(pkg)))
+    print(dim(function_names))
+  }
+}
+
