3b64dab31da5aa7c48f590e026d94c1eb0b78b62
esander91 <liz.sander91@gmail.com>
2016-04-08 18:22:00 -0500

    pkg_function file based on using ls.str instead of lsf.str

--- Code/RCode/one_pkg.R
+++ Code/RCode/one_pkg.R
@@ -5,6 +5,6 @@ library(pkg, character.only = TRUE)
 fns <- as.vector(ls.str(paste0('package:', pkg)))
 df <- data.frame(package = rep(pkg, length(fns)),
                  func = fns)
-write.table(df, "../../Data/pkg_function_2.txt",
+write.table(df, "../../Data/pkg_function_ls_str.txt",
             row.names = FALSE, col.names = FALSE,
             quote = FALSE, append = TRUE)
